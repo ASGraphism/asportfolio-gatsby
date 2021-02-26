30aaa14ac0980f7df6bf9f411d3c97fc56d8c6b0
Aurore Salavert <ausalavert@gmail.com>
2021-02-26 10:58:43 +0100

    Revert "Revert "debug show images""
    This reverts commit 6d61b6fac66116b9080f9c62fd7a413899e0206b.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
+const path = require("path");
+
 module.exports = {
-  siteMetadata: {
-    title: "frontend",
-  },
-  plugins: ["gatsby-plugin-sass"],
+	siteMetadata: {
+		title: "frontend",
+	},
+	plugins: [
+		"gatsby-plugin-sass",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "assets",
+				path: path.join(__dirname, "src", "assets"),
+			},
+		},
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`,
+	],
 };
